38925293acd31d48b927869cd19182af734e54c5
Juan Camilo Liberato <jcliberatol@unal.edu.co>
2015-08-03 16:27:23 -0500

    added file based simulation

--- R/simulation.R
+++ R/simulation.R
@@ -1,3 +1,5 @@
+
+
 #' SimulateTest.
 #' Simulates a test according to a model
 #' 
@@ -8,6 +10,7 @@
 #' @param items the number of items to simulate
 #' @param individuals the number of individuals to simulate
 #' @param reps The number of tests to generate with this settings
+#' @param filename A directory where tests will be exported to.
 #' @param latentTraits
 #' @param independent Set this to false if all the individuals used to simulate the test must be the same
 #' @param dims Optional. The number of dimensions to simulate in the test if the model is multidimensional TODO (Untested in multidimensional, please do not use this parameter for now)
@@ -17,8 +20,30 @@
 #' @param verbose Optional. If true, output is made to know the status of the algorithm
 #' @examples
 #' k=simulateTest(items=20,individuals=2000,threshold=0.01,dims=1,reps=3,model="3PL")
-simulateTest<-function(model="2PL",items=10,individuals=1000,reps=1,dims=1,boundaries=NULL,generated=TRUE,itempars=NULL,latentTraits=NULL,seed=NULL,verbose=F,threshold=0)
+simulateTest<-function(model="2PL",items=10,individuals=1000,reps=1,dims=1,directory=NULL,boundaries=NULL,generated=TRUE,itempars=NULL,latentTraits=NULL,seed=NULL,verbose=F,threshold=0)
 { 
+  dirflag=F;
+  if(!is.null(directory)){
+    print.sentence("Outputting to directory",directory,verbose=verbose)
+    dirflag = T;
+  }
+  
+  cells = items*individuals*reps;
+  print.sentence("Total cells to simulate : ",cells,verbose=verbose)
+  groups=ceiling(cells/10000000)
+  gsize = floor(individuals/groups)
+  lsize = individuals - (groups*gsize);
+  if(groups == 1) lsize=individuals;
+  print.sentence("Groups in simulation : ",groups, "size : ", gsize, verbose=verbose , " last group size : ", lsize)
+  
+  
+  
+  ##If cells are 1 or less, proceed as normal, if not create the files and break the process.
+  
+  ##Breaking flag
+  #If the breaking flag is activated , 
+  ##Dir flag
+  
   model=irtpp.model(model)
   #TODO Implement multidimensional test simulation
   dims=1
@@ -29,7 +54,7 @@ simulateTest<-function(model="2PL",items=10,individuals=1000,reps=1,dims=1,bound
   set.seed(seed);
   ret$seed = seed;
   check.model(model);
-  #Generate the persons parameters (or read)
+  #Generate the item parameters (or read)
   z = ua(itempars,simulateItemParameters(items,model,dims,boundaries));
   ret$itempars = z;
   #Generate the individual parameters (assume normal for now, change later)
@@ -37,45 +62,105 @@ simulateTest<-function(model="2PL",items=10,individuals=1000,reps=1,dims=1,bound
   th=(th-mean(th))/sd(th)
   th = matrix(th,ncol=dims);
   ret$latentTraits = th
-  #Generate the tests
-  if(verbose){print("Starting simulation")}
-  ret$prob=replicate(reps,do.call(rbind,lapply(th,function(x,z) probability.3pl(theta=x,z=z),z=z)),simplify=F)
-  coins=replicate(reps,matrix(runif(items*individuals),ncol=items),simplify=F);
-  ret$test=mapply(function(x,y){ifelse(x>y,1,0)},ret$prob,coins,SIMPLIFY=F);
-  if(verbose){print("Simulation finished ... ")}
-  #Impute the test to exclude individuals in the threshold
-  repeat{
-    #scores of individuals and items
-    if(verbose){print("")}
-    individual.scores = lapply(ret$test,function(x) rowSums(x)/items);
-    #outlier scores
-    outliers.flags = lapply(individual.scores,function(x) ifelse(x<threshold | x>(1-threshold),T,F))
-    outliers.indices = lapply(outliers.flags,function(x) as.list(which(x)))
-    outliers.missing = lapply(outliers.indices,length)
-    outliers.total = Reduce(sum,outliers.missing)
-    if(outliers.total<2){
-      print.sentence("No outliers left",verbose=verbose)
-      break
-    }
-    else{
-      print.sentence("Outliers left",outliers.total,verbose=verbose)
-    }
-    #resimulate the coins
-    if(verbose){print("Resimulating coins ...")}
-    newcoins = sapply(outliers.missing,function(x){matrix(runif(x*items),ncol=items)},simplify=F)
-    probs = mapply(function(x,y){x[as.numeric(y),]},ret$prob,outliers.indices,SIMPLIFY=F)
-    if(verbose){print("Calculating new scores ...")}
-    newscores=mapply(function(x,y){ifelse(x>y,1,0)},probs,newcoins,SIMPLIFY=F);
-    #assign the new scores in the the old test
-    if(verbose){print("Re-assigning new scores ...")}
-    mapply(function(x,y,z){
-      if(outliers.missing[[z]]>0){
-        mapply(function(a,b){
-          ret$test[[z]][a,]<<-y[b,]
-        },x,seq(length(x)),SIMPLIFY=F);
-      }
-    },outliers.indices,newscores,seq(length(outliers.indices)),SIMPLIFY=F);
+  th=NULL;
+  gc()
+  individuals = gsize;
+  ##Break the simulation here in files.
+  if(dirflag){setwd(dir=directory)}
+  for (j in 1:reps){
+  fname = paste0("test",j,".csv");
+  if(file.exists(fname)){
+    print.sentence("Deleting file",fname);
+    file.remove(fname)}
   }
+  for (i in 1:groups){
+    
+    if(i == groups){
+      individuals = lsize
+    }
+    ##Select only the thetas in this file reduction.
+    print.sentence(((i-1)*gsize)+1,(((i-1)*gsize))+individuals)
+    print.sentence("Length of traits",length(ret$latentTraits))
+    b1 = ((i-1)*gsize)+1
+    b2 = (((i-1)*gsize))+individuals
+    th = ret$latentTraits[b1:b2];
+    
+    if(verbose){print("Starting simulation")}
+    ##Here th must be exactly the latent traits of these individuals in this test.
+    ret$prob=replicate(reps,do.call(rbind,lapply(th,function(x,z) probability.3pl(theta=x,z=z),z=z)),simplify=F)
+    gc()
+    coins=replicate(reps,matrix(runif(items*individuals),ncol=items),simplify=F);
+    gc()
+    ret$test=mapply(function(x,y){ifelse(x>y,1,0)},ret$prob,coins,SIMPLIFY=F);
+    coins=NULL
+    gc()
+    if(verbose){print("Simulation finished ... ")}
+    #Impute the test to exclude individuals in the threshold
+    repeat{
+      #scores of individuals and items
+      if(verbose){print("")}
+      individual.scores = lapply(ret$test,function(x) rowSums(x)/items);
+      #outlier scores
+      outliers.flags = lapply(individual.scores,function(x) ifelse(x<threshold | x>(1-threshold),T,F))
+      outliers.indices = lapply(outliers.flags,function(x) as.list(which(x)))
+      outliers.missing = lapply(outliers.indices,length)
+      outliers.total = Reduce(sum,outliers.missing)
+      if(outliers.total<2){
+        print.sentence("No outliers left",verbose=verbose)
+        gc()
+        break
+      }
+      else{
+        gc()
+        print.sentence("Outliers left",outliers.total,verbose=verbose)
+      }
+      #resimulate the coins
+      if(verbose){print("Resimulating coins ...")}
+      newcoins = sapply(outliers.missing,function(x){matrix(runif(x*items),ncol=items)},simplify=F)
+      probs = mapply(function(x,y){x[as.numeric(y),]},ret$prob,outliers.indices,SIMPLIFY=F)
+      if(verbose){print("Calculating new scores ...")}
+      newscores=mapply(function(x,y){ifelse(x>y,1,0)},probs,newcoins,SIMPLIFY=F);
+      #assign the new scores in the the old test
+      if(verbose){print("Re-assigning new scores ...")}
+      mapply(function(x,y,z){
+        if(outliers.missing[[z]]>0){
+          mapply(function(a,b){
+            ret$test[[z]][a,]<<-y[b,]
+          },x,seq(length(x)),SIMPLIFY=F);
+        }
+      },outliers.indices,newscores,seq(length(outliers.indices)),SIMPLIFY=F);
+    }
+    gc()
+    ##Handle the return
+    ##Output to the file.
+    print.sentence("Outputting to files ",verbose=verbose)
+    for (j in 1:reps){
+      fname = paste0("test",j,".csv");
+      write.table(ret$test[[j]],file=fname,append=T,sep=",",col.names=F,row.names=F)
+    }
+    print.sentence("... ",verbose=verbose)
+    
+  }
+  ##Delete the files if option is not enabled.
+  ##top("hammertime")
+  if(dirflag){
+    ret$test=directory;
+  }
+  else {
+    for (j in 1:reps){
+        fname = paste0("test",j,".csv");
+        path = paste0(getwd(),"/",fname)
+        ts=read.table(file=fname,header=F,sep=",")
+        ts = data.matrix(ts,rownames.force=F)
+        ret$test[[j]]=ts;
+        ts=NULL;
+        gc()
+        file.remove(fname);
+    }
+  }
+  #Generate the tests
+  ret$prob = NULL;
+  gc();
   ret
 }
 
