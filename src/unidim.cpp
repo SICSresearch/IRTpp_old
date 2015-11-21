28d6e33e994c8771c163d5601d3ec4c64078e9a2
Liberato <jcliberatol@github.com>
2015-11-21 13:49:28 -0500

    debugging new interface

--- src/unidim.cpp
+++ src/unidim.cpp
@@ -24,63 +24,71 @@ using std::cout;
 using std::endl;
 
 //Please reference dat memory before this or set pointer to NULL.
-void mat2dat(Rcpp::NumericMatrix mat , irtpp::dataset* dat ){
-        if (dat == NULL) {
-                dat = new irtpp::dataset(0);
-        }
-        for (size_t r = 0; r < mat.nrow(); r++) {
-                std::vector<char>  v(mat.ncol());
-                for (size_t c = 0; c < mat.ncol(); c++) {
-                        int tmp = (int)(mat[c*mat.nrow()+r]);
-                        if(tmp==1){v[c] = true;}
-                        else{v[c]=false;}
-                        //cout<<"c : "<<c<<" _ "<<v[c]<<"ñ"<<mat[c*mat.nrow()+r]<<endl;
-                        dat->size = mat.ncol();
-                        //cout<<"s : "<<dat->size<<endl;
-                        dat->push(v);
-                }
-        }
+irtpp::dataset* mat2dat(Rcpp::NumericMatrix mat)
+{
+  irtpp::dataset* dat;
+  
+  dat = new irtpp::dataset(0);
+  
+  for (int r = 0; r < mat.nrow(); r++)
+  {
+    std::vector<char> v(mat.ncol());
+    
+    for (int c = 0; c < mat.ncol(); c++)
+    {
+      v[c] = mat[c*mat.nrow()+r];
+    }
+    
+    dat->size = mat.ncol();
+    dat->push(v);
+  }
+  
+  dat->size = mat.ncol();
+  
+  return dat;
 }
 
-Rcpp::NumericMatrix mat2rcpp(Matrix<double>* mat){
-        Rcpp::NumericMatrix x(mat->nR(),mat->nC());
-        for (size_t i = 0; i < x.nrow() ; i++) {
-                for (size_t j = 0; j < x.ncol(); j++) {
-                        x[i*x.nrow()+j*x.ncol()] = (*mat)(i,j);
-                }
-        }
-        return x;
+Rcpp::NumericMatrix mat2rcpp(Matrix<double>* mat)
+{
+  Rcpp::NumericMatrix x(mat->nR(),mat->nC());
+  
+  for (size_t i = 0; i < x.nrow() ; i++)
+  {
+    for (size_t j = 0; j < x.ncol(); j++)
+    {
+      x[i*x.nrow()+j*x.ncol()] = (*mat)(i,j);
+    }
+  }
+  
+  return x;
 }
 
 //' uirtestimate
 //' @export
 // [[Rcpp::export]]
-Rcpp::List uirtestimate(Rcpp::NumericMatrix data , int model){
+Rcpp::List uirtestimate(Rcpp::NumericMatrix data , int model)
+{
   // Create Dataset.
   void**   status_list;
-  cout<<"un"<<endl;
   irtpp::dataset* d = NULL;
-  cout<<"dos"<<d<<endl;
-  mat2dat(data,d);
-  cout<<"tres"<<d<<endl;
+  d = mat2dat(data);
   irtpp::model* m = NULL;
-  d->print();
-  cout<<"cuatro"<<d->size<<endl;
-
-   irtpp::emestimation em(new irtpp::threepl(),   d);
-cout<<"cinco"<<endl;
+  // d->print();
+  
+  irtpp::emestimation em(new irtpp::threepl(), d);
   status_list = em.estimate();
-  cout<<"coef"<<endl;
+  cout<<"seis1"<<endl;
   Matrix<double>* args = em.coef();
-cout<<"seis"<<endl;
-  Rcpp::List pars = Rcpp::List::create(Rcpp::_["args"] = mat2rcpp(args),
-                                       Rcpp::_["iterations"] = *((int*)status_list[0])
-                               );
-
+  cout<<"seis2"<<endl;
+  Rcpp::List pars = Rcpp::List::create(
+    Rcpp::_["args"] = mat2rcpp(args),
+    Rcpp::_["iterations"] = *((int*)status_list[0])
+  );
+  
   cout << "Iterations: " << *((int*)status_list[0]) << endl;
   delete (int*)status_list[0];
   delete (bool*)status_list[1];
   delete [] status_list;
   return pars;
-
+  
 }
